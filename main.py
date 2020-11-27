1a8740307e969ca9cf736d706e1756ad1fb06d82
Victor-BlackPC <victoraguirre.f@gmail.com>
2020-11-27 14:47:34 +0800

    20201127 Adding Main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import string
+import secrets
+
+def pass_generator(size: int) -> str:
+	alphabet = string.ascii_letters + string.digits + string.punctuation
+	while True:
+		password = ''.join(secrets.choice(alphabet) for i in range(size))
+		if (any(c.islower() for c in password) and any(c.isupper() for c in password) and sum(c.isdigit() for c in password) >= size//2):
+			break
+	return password
+
+if __name__ == "__main__":
+	while True:
+		try:
+			size = int(input("input password size: ")) or 8
+			print(f'\n* Password:\n{pass_generator(size)}')
+			break
+		except ValueError:
+			print("wrong value, please input a numeric value")
