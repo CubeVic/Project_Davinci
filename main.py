8f9c037de06d33b6fc09b0d0b6ae30648e5756e8
Victor-BlackPC <victoraguirre.f@gmail.com>
2020-11-27 16:24:05 +0800

    20201127 Update:  Formatting

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ def pass_generator(size: int) -> str:
 	alphabet = string.ascii_letters + string.digits + string.punctuation
 	while True:
 		password = ''.join(secrets.choice(alphabet) for i in range(size))
-		if (any(c.islower() for c in password) and any(c.isupper() for c in password) and sum(c.isdigit() for c in password) >= size//2):
+		if (any(c.islower() for c in password) and 
+			any(c.isupper() for c in password) and 
+			sum(c.isdigit() for c in password)) >= size//2:
 			break
 	return password
 
 if __name__ == "__main__":
 	while True:
 		try:
-			size = int(input("input password size: ")) or 8
+			size = int(input("input password size: "))
 			print(f'\n* Password:\n{pass_generator(size)}')
 			break
 		except ValueError:
